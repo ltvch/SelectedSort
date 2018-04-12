adf6c96e34a3a744e0b1dbbf26886861a317f220
ltvch <HrenHrenov@myrambler.ru>
2018-04-12 09:44:56 +0300

    Write selected sort on C#

--- SelectedSort/Program.cs
+++ SelectedSort/Program.cs
@@ -4,6 +4,23 @@ namespace SelectedSort
 {
     class Program
     {
+        static void SelectedSort(int[] array)
+        {
+            int min, temp;
+            
+            for(int i = 0; i<array.Length - 1;i++)
+            {
+                min = i;
+                for (int j = i + 1; j < array.Length; j++)
+                    if (array[j] < array[min])
+                        min = j;
+
+                //swap the values
+                temp = array[i];
+                array[i] = array[min];
+                array[min] = temp;
+            }
+        }
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
