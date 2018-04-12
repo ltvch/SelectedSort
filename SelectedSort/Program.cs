89a8ee07e4b4f7b8b9821dcbbe1eb841b282b3d0
ltvch <HrenHrenov@myrambler.ru>
2018-04-12 09:38:38 +0300

    Добавьте файлы проекта.

---
+++ SelectedSort/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SelectedSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
