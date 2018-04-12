905a382896c6c50c1d2f502dcc3b5b8f00947da5
ltvch <HrenHrenov@myrambler.ru>
2018-04-12 09:57:09 +0300

    Add run in console

--- SelectedSort/Program.cs
+++ SelectedSort/Program.cs
@@ -23,7 +23,16 @@ namespace SelectedSort
         }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] array = new int[] { 4, 5, 3, 8, 1, 3, 7, 9, 0, 0 };
+            SelectedSort(array);
+
+            foreach(int item in array)
+            {
+                Console.Write(item);
+                Console.Write(" ");
+            }
+
+            Console.Read();
         }
     }
 }
